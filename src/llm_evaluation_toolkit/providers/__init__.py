9543356c9135b1a937983002239329b37612ef9f
Eugenio Fontenla Suárez <301641985+eugefs@users.noreply.github.com>
2026-07-22 14:20:09 +0200

    feat: add Anthropic provider

--- src/llm_evaluation_toolkit/providers/__init__.py
+++ src/llm_evaluation_toolkit/providers/__init__.py
@@ -1,10 +1,13 @@
 """Provider abstractions."""
 
+from .anthropic import AnthropicProvider, AnthropicProviderConfig
 from .async_generator import AsyncGenerator
 from .base import Provider
 from .generator import Generator
 
 __all__ = [
+    "AnthropicProvider",
+    "AnthropicProviderConfig",
     "AsyncGenerator",
     "Generator",
     "Provider",
